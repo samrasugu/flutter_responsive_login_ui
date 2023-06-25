0a0ecd555611d2bb3987075fd0f078f1f7713c6a
samrasugu <mokuasamr@gmail.com>
2023-06-25 11:37:46 +0300

    Final commit

---
+++ lib/widgets/social_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive_login_ui/pallete.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SocialButton extends StatelessWidget {
+  final String iconPath;
+  final String label;
+  final double horizontalPadding;
+  final VoidCallback onPressed;
+  const SocialButton({
+    super.key,
+    required this.iconPath,
+    required this.label,
+    this.horizontalPadding = 100,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton.icon(
+      onPressed: onPressed,
+      icon: SvgPicture.asset(
+        iconPath,
+        width: 25,
+        color: Pallete.whiteColor,
+      ),
+      label: Text(
+        label,
+        style: const TextStyle(
+          fontSize: 17,
+          color: Pallete.whiteColor,
+        ),
+      ),
+      style: TextButton.styleFrom(
+        padding: EdgeInsets.symmetric(
+          vertical: 30,
+          horizontal: horizontalPadding,
+        ),
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(
+            color: Pallete.borderColor,
+            width: 3,
+          ),
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+}
